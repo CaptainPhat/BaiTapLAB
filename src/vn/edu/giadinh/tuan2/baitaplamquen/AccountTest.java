2c68d1c1bfa507dd2b6e046287bf3c783317e2c0
captainphat <nphat2891@gmail.com>
2021-09-30 12:31:56 +0700

    baitest

---
+++ src/vn/edu/giadinh/tuan2/baitaplamquen/AccountTest.java
@@ -0,0 +1,12 @@
+/*
+* create date: 30 thg 9, 2021
+* author: captainPhat
+*/
+package vn.edu.giadinh.tuan2.baitaplamquen;
+
+public class AccountTest {
+   public static void main(String[] args) {
+       Account account = new Account(2008110174,500);
+   }
+    
+}
