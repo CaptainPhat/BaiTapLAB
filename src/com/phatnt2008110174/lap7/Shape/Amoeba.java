9ca1b74d38fb94777d1ec05bfd654581e5ece6f5
captainphat <nphat2891@gmail.com>
2021-10-27 11:23:51 +0700

    á

--- src/com/phatnt2008110174/lap7/Shape/Amoeba.java
+++ src/com/phatnt2008110174/lap7/Shape/Amoeba.java
@@ -5,19 +5,19 @@
 package com.phatnt2008110174.lap7.Shape;
 
 public class Amoeba extends Shape {
-    int x;
-    int y;
+  protected int x;
+  protected int y;
    
-    Amoeba(int xx, int yy,String s){
+   protected Amoeba(int xx, int yy,String s){
         x = xx;
         y = yy;
         Sound = s;
     }
-     public void Rotate()
+    protected void Rotate()
      {
        System.out.println("da xay 360 voi toa do x= "+ x + " va y = "+ y);
      }
-    public void playSound()
+     protected void playSound()
      {
         System.out.println("insert am thanh .HIF: "+ Sound);
      }
