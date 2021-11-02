c7a057f64cb190f2961825a66dc1bdf6f7b318d7
captainphat <nphat2891@gmail.com>
2021-11-02 22:42:27 +0700

    lab8

---
+++ src/com/phatnt2008110174/lap8/SinhVien/SinhVienBiz.java
@@ -0,0 +1,18 @@
+/*
+* create date: 02 thg 11, 2021
+* author: captainPhat
+*/
+package com.phatnt2008110174.lap8.SinhVien;
+
+public class SinhVienBiz extends SinhVien {
+    private double  Marketting,Sales;
+    public SinhVienBiz(String hoTen, Double Marketting,Double Sales) {
+        super(hoTen,hoTen);
+        this.Marketting = Marketting;
+        this.Sales = Sales;
+        //TODO Auto-generated constructor stub
+    }
+   protected double getDiem(){
+       return (2* Marketting + Sales)/3;
+   }
+}
---
+++ src/com/phatnt2008110174/lap8/SinhVien/SinhVienIT.java
@@ -0,0 +1,20 @@
+/*
+* create date: 02 thg 11, 2021
+* author: captainPhat
+*/
+package com.phatnt2008110174.lap8.SinhVien;
+
+public class SinhVienIT extends SinhVien {
+    private double Diem, Java,Html,Css;
+    public SinhVienIT(String hoTen,Double Java,Double Html,Double Css) {
+        super(hoTen, hoTen);
+        this.Java = Java;
+        this.Html = Html;
+        this.Css = Css;
+        //TODO Auto-generated constructor stub
+    }
+    protected double getDiem() {
+        return (2*Java + Html + Css )/4;
+    }
+    
+}
